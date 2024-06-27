f69cbd1660964e965ea0dd55b2e43527ff2beff6
sakhawat777 <biplobbms@gmail.com>
2024-06-27 17:08:02 +0600

    Set Theme in localStorage

--- components/useTheme .js
+++ components/useTheme .js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 
 export const useTheme = (initialTheme = 'light') => {
-	const [theme, setTheme] = useState(initialTheme);
+	const [theme, setTheme] = useState(() => {
+		const savedTheme = localStorage.getItem('theme');
+		return savedTheme ? savedTheme : initialTheme;
+	});
 
 	const toggleTheme = () => {
 		setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
@@ -11,6 +14,7 @@ export const useTheme = (initialTheme = 'light') => {
 		document.body.style.backgroundColor =
 			theme === 'dark' ? 'black' : 'white';
 		document.body.style.color = theme === 'dark' ? 'white' : 'black';
+		localStorage.setItem('theme', theme);
 	}, [theme]);
 
 	return { theme, toggleTheme };
